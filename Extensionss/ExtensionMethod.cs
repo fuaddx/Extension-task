3298c7f4ede28c12e34aaa8c395f51bcaa1c789a
Fuad <mi848xfcl@code.edu.az>
2023-11-02 02:07:02 +0400

    Extensions

---
+++ Extensionss/ExtensionMethod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensionss
+{
+        public static class ExtensionMethod
+        {
+            public static bool Isprime(this int number)
+            {
+                bool isPrime = true;
+
+                for (int i = 0; i <= number; i++)
+                {
+                    for (int j = 2; j <= number; j++)
+                    {
+                        if (i != j && i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    isPrime = true;
+                }
+                return isPrime;
+
+            }
+
+            public static bool IsPowOfTwo(this int number)
+            {
+                if (number <= 0)
+                    return false;
+
+                while (number > 1)
+                {
+                    if (number % 2 != 0) return false;
+
+                    number /= 2;
+                }
+
+                return true;
+            }
+        }
+    
+}
---
+++ Extensionss/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensionss
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+
+            bool isPrime = number.Isprime();
+            bool powoftwo = number.IsPowOfTwo();
+
+            if (!isPrime)
+            {
+                Console.WriteLine($"{number} is prime.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not prime.");
+            }
+            //pow of 2
+            if (powoftwo)
+            {
+                Console.WriteLine($"{number} is Pow Of 2.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not Pow Of Two.");
+            }
+
+        }
+    }
+}
